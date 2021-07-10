b880a1812856e33fa2f8e960c268adec416c345b
Derek Widmer <dwidmer99@gmail.com>
2021-07-10 01:36:22 -0400

    Finished lotto functionality and styles

---
+++ src/Lottery.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import LotteryBall from './LotteryBall'
+import './Lottery.css'
+
+class Lottery extends Component {
+    state = {
+        nums: [1, 1, 1, 1]
+    }
+
+    generate = () => {
+        let newNums = []
+        for (let x = 0; x < this.props.numBalls; x++) {
+            newNums.push(Math.floor(Math.random(this.props.maxNum) * 10) + 1)
+        }
+        this.setState({ nums: newNums })
+    }
+
+    render() {
+
+        return (
+            <div className="Lottery">
+                <h1>{this.props.title}</h1>
+                <div className="Lottery-balls">
+                    {this.state.nums.map(n => <LotteryBall num={n} />)}
+                </div>
+                <button onClick={this.generate}>Generate</button>
+            </div>
+        )
+    }
+}
+
+export default Lottery;
